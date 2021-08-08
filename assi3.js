1c0c22a54bd0e0032f43b53b7e7972a2d1321b17
techsurfer9 <38426693+techsurfer9@users.noreply.github.com>
2021-08-08 20:55:33 +0530

    ass3
    ass3

---
+++ assi3.js
@@ -0,0 +1,41 @@
+let anime = []
+    anime.name;
+    anime.release;
+    anime.year;
+    anime.genre;
+
+
+
+let anime1 = []
+anime1.name = "Fullmetal Alchemist: Brotherhood";
+anime1.release = "September 10";
+anime1.year = "5 April 2009";
+anime1.genre = " Anime, Dark fantasy, Action fiction, Steampunk, Drama";
+
+let anime2 = []
+anime2.name = "Naruto: Shippuden";
+anime2.release = "15 February";
+anime2.year = " 2007";
+anime2.genre = "Comedy,Drama,Action";
+
+let anime3 = []
+anime3.name = "Attack on Titan";
+anime3.release = "7 April ";
+anime3.year = "2013";
+anime3.genre = "Action,Crime";
+
+let anime4 = []
+anime4.name = "Code Geass";
+anime4.release = "5 October";
+anime4.year = "2006";
+anime4.genre = "Action-adventure,drama";
+
+anime.push(anime1);
+anime.push(anime2);
+anime.push(anime3);
+anime.push(anime4);
+
+console.log(anime[0]);
+console.log(anime[1]);
+console.log(anime[2]);
+console.log(anime[3]);
